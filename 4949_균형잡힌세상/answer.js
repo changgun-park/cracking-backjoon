1d3b1041fc086ffc4dd4c93ae3c3677c20eb4a31
changgun-park <pcg0537@gmail.com>
2023-06-06 11:11:41 +0900

    4949

--- 4949_균형잡힌세상/answer.js
+++ 4949_균형잡힌세상/answer.js
@@ -1,30 +1,22 @@
-const lines = require("fs")
+const result = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
   .toString()
   .trim()
   .split("\n")
-  .slice(0, -1);
-
-const matcher = {
-  ")": "(",
-  "]": "[",
-};
-
-const result = [];
-for (const line of lines) {
-  const stack = [];
-  for (const str of line) {
-    if (str === "(" || str === "[") {
-      stack.push(str);
-    }
-    if (str === ")" || str === "]") {
-      if (stack[stack.length - 1] === matcher[str]) {
-        stack.pop();
-      } else {
-        stack.push(str);
+  .slice(0, -1)
+  .map((line) => {
+    const stack = [];
+    for (const str of line) {
+      switch (str) {
+        case "(":
+        case "[":
+          stack.push(str === "(");
+        case ")":
+        case "]":
+          if (stack.pop() !== (str === ")")) return "no";
       }
     }
-  }
-  if (stack.length) result.push("no");
-  else result.push("yes");
-}
+    return stack.length > 0 ? "no" : "yes";
+  });
+
+console.log(result.join("\n"));
